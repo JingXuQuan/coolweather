490f260e6c3611198f33b81b897b1e6cf605d5a4
XuQuan <409147568@qq.com>
2017-10-13 11:07:26 +0800

    加入显示天气信息得功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by JXQ on 2017/10/10.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
